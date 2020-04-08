8d821322b0488844ecf330bae8d3ce64e1ab77b8
AI Birds <sailon.aibirds@gmail.com>
2020-04-08 13:52:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import sys
 
 import src.demo.naive_agent_groundtruth as na
-import src.demo.ddqn_agent as ddq
 
 def main(*args):
-	if ('-dq' in sys.argv):
-		print("ddq running")
-		agent = ddq.ClientRLAgent()
-	else:
-		print ('naive agent running')
-		agent = na.ClientNaiveAgent()
+	print ('naive agent running')
+	agent = na.ClientNaiveAgent()
 	agent.run()
 
 if __name__ == "__main__":
